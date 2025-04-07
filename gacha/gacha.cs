bcaf1d09b9a0cab37f28dfb8b0b162b27706c83e
則巻おかき (Norimaku Okaki) <44014538+okakiya@users.noreply.github.com>
2025-04-07 21:39:05 +0900

    Update gacha.cs
    Add: とりあえずエラー消した

--- gacha/gacha.cs
+++ gacha/gacha.cs
@@ -1,7 +1,9 @@
+using UnityEngine;
 using Warudo.Core.Attributes;
 using Warudo.Core.Data;
 using Warudo.Core.Graphs;
 using Warudo.Core.Localization;
+using System.Collections.Generic;
 
 namespace Warudo.Plugins.Core.Nodes {
 
@@ -15,49 +17,32 @@ namespace Warudo.Plugins.Core.Nodes {
             RARE = rare;
         }
 
-        public int GACHA_ID { get; init; }
-        public int ITEM_NUMBER  { get; init; }
-        public string ITEM_NAME { get; init; }
-        public float WEIGHT { get; init; }
-        public int RARE { get; init; }
-
-        public override string ToString() => $"{GACHA_ID},{ITEM_NUMBER},{ITEM_NAME},{WEIGHT},{RARE}"
+        public int GACHA_ID { get; }
+        public int ITEM_NUMBER  { get; }
+        public string ITEM_NAME { get; }
+        public float WEIGHT { get; }
+        public int RARE { get; }
     }
 
     // ガチャデータベース
-    public struct GachaDatabaseStruct {
-        public GachaDatabaseStruct() {
-            GACHA_ITEM_LIST = new Dictionary<(int, int), GachaItem[]>();
+    public class GachaDatabase {
+        public GachaDatabase() {
+            this.GACHA_ITEM_LIST = new Dictionary<(int, int), GachaItem>();
         }
 
-        public Dictionary<(int, int), GachaItem[]> GACHA_ITEM_LIST { get; set; }
+        public void Add(int gachaId, int itemNumber, GachaItem gachaItem) {
+            this.GACHA_ITEM_LIST.Add((gachaId, itemNumber), gachaItem);
+        }
+
+        public GachaItem Get() {
+            return this.GACHA_ITEM_LIST[(0,0)];
+        }
+
+        public Dictionary<(int, int), GachaItem> GACHA_ITEM_LIST { get; set; }
     }
 
-    public static class GachaDatabase {
-        public static GachaDatabaseStruct data { get; set; };
-
-        public static add(GachaItem gachaItem) {
-            gachaId = gachaItem.GACHA_ID;
-            gachaItemNum = gachaItem.ITEM_NUMBER;
-            if (data.GACHA_ITEM_LIST.Contains(gachaId, gachaItemNum)) {
-                self.Remove(gachaId, gachaItemNum);
-            }
-            data.GACHA_ITEM_LIST.Add((gachaId, gachaItemNum), gachaItem);
-        }
-
-        public static remove(int gachaId, int gachaItemNum) {
-            data.GACHA_ITEM_LIST.Remove(gachaId, gachaItemNum);
-        }
-
-        public static get(int gachaId, int gachaItemNum) {
-            // TODO バリデーション
-            return data.GACHA_ITEM_LIST[gachaId][gachaItemNum];
-        }
-
-        public static destroy() {
-            // TODO 明示的にディクショナリは捨てた方がいいか？
-            data.GACHA_ITEM_LIST = new Dictionary<(int, int), GachaItem[]>();
-        }
+    public static class GachaDatabaseManage {
+        public static GachaDatabase data = new GachaDatabase();
     }
 
     // TODO カテゴリーをなんとかする
@@ -66,15 +51,11 @@ namespace Warudo.Plugins.Core.Nodes {
         Title = "setGachaItemList",
         Category = "Gacha"
     )]
-    public class SetGachaItemList : Nodes {
+    public class SetGachaItemList : Node {
 
         [DataInput]
         [Label("GACHA_ITEM_LIST")]
-        public string[] GachaItemList;
-
-        [DataOutput]
-        [Label("DEBUG_GACHA")]
-        public string[] DebugGachaList; // でバック用
+        public string GachaItemList;
 
         public int counter = 0; // でバック用
 
@@ -85,37 +66,7 @@ namespace Warudo.Plugins.Core.Nodes {
         }
         
         public void SetupGachaDatabase() {
-            GachaDatabase.destroy();
-            foreach (string gachaItemStr in GachaItemList) {
-                string[] gachaItemPropList = gachaItemStr.Split(',');
-                // TODO バリデーション, ガチャID-景品Noユニーク判定
-                Int32.TryParse(gachaItemPropList[0], out int id);
-                Int32.TryParse(gachaItemPropList[1], out int num);
-                string itemName = gachaItemPropList[2];
-                float.TryParse(gachaItemPropList[3], out float weight);
-                Int32.TryParse(gachaItemPropList[4], out int rare);
-                GachaDatabase.add(
-                    GachaItem(id, num, itemName, weight, rare);
-                );
-            }
-            GachaDatabase.get(0,0); // TODO
-        }
-    }
-
-    // TODO カテゴリーをなんとかする
-    [NodeType(
-        Id = "c8f02de5-169c-44f1-9466-598e01e68389",
-        Title = "doGacha",
-        Category = "Gacha"
-    )]
-    public class DoGacha : Node {
-
-        [DataOutput]
-        [Label("GACHA_DATA")]
-        public string[] GachaData;
-
-        protected override void OnCreate() {
-            base.OnCreate();
+            GachaDatabaseManage.data.Add(0, 0, new GachaItem(0, 0, "testname", 0.5f, 2));
         }
     }
 }
